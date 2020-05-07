bd83dd3347b715f64f6dd3326e4bfa19981fe58c
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-07 21:37:15 +0430

    create sidebar component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 const Home = () => {
     return (
@@ -27,14 +28,7 @@ const Home = () => {
                 <div className="row">
 
                     <div className="col-lg-3">
-
-                        <h1 className="my-4">Shop Name</h1>
-                        <div className="list-group">
-                            <a href="#" className="list-group-item">Category 1</a>
-                            <a href="#" className="list-group-item">Category 2</a>
-                            <a href="#" className="list-group-item">Category 3</a>
-                        </div>
-
+                        <Sidebar/>
                     </div>
 
                     <div className="col-lg-9">
